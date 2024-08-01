96be53b4bcd95051daee613442f46bba96fe116e
adionizio14 <aadion14@gmail.com>
2024-08-01 14:54:07 -0400

    idk

---
+++ pd.py
@@ -0,0 +1,5 @@
+from pipedrive.client import Client
+
+client = Client(domain='https://specifiedbuildingproducts-661b09.pipedrive.com/')
+client.set_api_token('a7eeff293944a9925472c842fe7ca8330c7eade6')
+
