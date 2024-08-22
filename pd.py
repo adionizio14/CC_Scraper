7722b5865475d09523d654104ee06c6046a71c8b
adionizio14 <aadion14@gmail.com>
2024-08-22 14:48:10 -0400

    [pd] added multiple methods for creating new deal

--- pd.py
+++ pd.py
@@ -9,12 +9,71 @@ class Pipedrive:
         Initalizes the users API token
         """
         
-        authenticator = auth.Authentication()
+        self.authenticator = auth.Authentication()
 
         self.client = Client(domain='https://specifiedbuildingproducts-661b09.pipedrive.com/')
-        self.client.set_api_token(authenticator.token)
+        self.client.set_api_token(self.authenticator.token)
+
+    def get_state(self, address):
+        parts = address.split(',')
+        if len(parts) > 2:
+            state_zip = parts[2].strip().split()
+            if len(state_zip) > 0:
+                self.state = state_zip[0]
+        
+    def get_company_details(self, participants):
+
+        self.architect = ""
+        self.lead_architect = ""
+        self.general_contractor = ""
+        self.lead_gc = ""
+
+        for entry in participants:
+            # print(entry)
+            if entry['Company Role'] == 'Architect' or entry['Company Role'] == 'Awarded - Architect':
+                self.architect = entry['Company Name']
+                self.lead_architect = entry['Contact Name']
+            elif entry['Company Role'] == 'General Contractor':
+                self.general_contractor = entry['Company Name']
+                self.lead_gc = entry['Contact Name']
+    
+    def create_new_deal(self, title, participants, address, id):
+
+        self.get_company_details(participants)
+        self.get_state(address)
+
+        # gather data for deal
+
+        # create the deal
+
+        # get id of deal
+
+        data = self.update_deal_fields(id)
+
+        # update deal
+
+    def update_deal_fields(self, id):
+
+        data = {
+
+            self.authenticator.business_unit : 'SBC',
+            self.authenticator.deal_type : 'Commercial',
+            self.authenticator.lead_source: 'Construct Connect',
+            self.authenticator.business_rep: 'Todd Grant',
+            self.authenticator.state: self.state,
+            self.authenticator.architect: self.architect,
+            self.authenticator.lead_architect: self.lead_architect,
+            self.authenticator.general_contractor: self.general_contractor,
+            self.authenticator.CC_id: id
+        }
+        
+        return data
 
     def get_field_keys(self):
+
+        """
+        Method to get the keys for custom deal fields
+        """
 
         response = self.client.deals.get_deal_fields()
         data = response["data"]
