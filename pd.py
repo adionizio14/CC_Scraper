118171231622a20e63f8ae7126b7440e2ca7b5c6
adionizio14 <aadion14@gmail.com>
2024-08-27 14:14:04 -0400

    [pd] create deal now works with updated notes and activity

--- pd.py
+++ pd.py
@@ -1,5 +1,6 @@
 from pipedrive.client import Client
 import auth
+import time
 
 class Pipedrive: 
 
@@ -37,20 +38,89 @@ class Pipedrive:
                 self.general_contractor = entry['Company Name']
                 self.lead_gc = entry['Contact Name']
     
-    def create_new_deal(self, title, participants, address, id):
+    def create_new_deal(self, title, participants, address, id, task_text, notes):
 
         self.get_company_details(participants)
         self.get_state(address)
 
+        self.org_id = 23569 #Placeholder - Construct Connect
+        self.person_id = None
+
+        self.check_org_and_lead()
+
+        self.pipeline_id = 17
+        self.stage_id = 114
         # gather data for deal
+
+        data = {
+
+            'title' : title,
+            'org_id' : self.org_id,
+            'person_id' : self.person_id,
+            'pipeline_id' : self.person_id,
+            'stage_id' : self.stage_id
+        }
 
         # create the deal
 
+        self.client.deals.create_deal(data)
+
+        time.sleep(8)
+
         # get id of deal
+
+        deal_id = self.get_deal_id(title)
 
         data = self.update_deal_fields(id)
 
         # update deal
+
+        self.client.deals.update_deal(deal_id, data)
+
+        self.post_note(notes, deal_id, self.org_id)
+        self.post_task(task_text, deal_id, self.org_id)
+
+    def check_org_and_lead(self):
+
+        if self.architect != "":
+            self.org_id = self.get_org_id(self.architect)
+        elif self.general_contractor != "":
+            self.org_id = self.get_org_id(self.general_contractor)
+        
+        if self.lead_architect != "":
+            self.person_id = self.get_person_id(self.lead_architect)
+        elif self.general_contractor != "":
+            self.person_id = self.get_person_id(self.lead_gc)
+    
+    def get_person_id(self, name):
+
+        params = {
+            'term': name
+        }
+
+        person = self.client.persons.search_persons(params=params)
+        person_data = person['data']
+        if len(person_data["items"]) == 0:
+            return None
+        person_items = person_data['items'][0]
+        person_item = person_items['item']
+        person_id = person_item['id']
+        return person_id
+    
+    def get_org_id(self, name):
+
+        params = {
+            'term': name
+        }
+
+        org = self.client.organizations.search_organizations(params=params)
+        org_data = org['data']
+        if len(org_data["items"]) == 0:
+            return 23569
+        org_items = org_data['items'][0]
+        org_item = org_items['item']
+        org_id = org_item['id']
+        return org_id
 
     def update_deal_fields(self, id):
 
@@ -97,7 +167,7 @@ class Pipedrive:
         deal_id = deal_item['id']
         return deal_id
     
-    def post_task(self, task_text, deal_id):
+    def post_task(self, task_text, deal_id, org_id):
 
         """ 
         Method to post the created task in PD 
@@ -108,12 +178,13 @@ class Pipedrive:
             'type':'task',
             'note':task_text,
             'deal_id':deal_id,
-            'done':1
+            'done':1,
+            'org_id': org_id
         }
 
         reponse = self.client.activities.create_activity(data)
     
-    def post_note(self, notes, deal_id):
+    def post_note(self, notes, deal_id, org_id):
 
         """
         Method to post the created note in PD
@@ -122,7 +193,8 @@ class Pipedrive:
         data = {
             'content':notes,
             'deal_id':deal_id,
-            'pinned_to_deal_flag':1
+            'pinned_to_deal_flag':1,
+            'org_id': org_id
         }
 
         reponse = self.client.notes.create_note(data)
